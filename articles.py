36702c1d294b945ac6701791d45fa4d7e9e4c085
MikesoWeb <klionpsyx@gmail.com>
2021-05-25 14:37:32 +0300

    'first_commit'

---
+++ articles.py
@@ -0,0 +1,25 @@
+posts = [{'author': 'MikeSoWeb',
+          'title': 'Welcome to Flask’s documentation',
+          'content': 'Get started with Installation and then get an overview with the Quickstart. There is also a '
+                     'more detailed Tutorial that shows how to create a small but complete application with Flask. '
+                     'Common patterns are described in the Patterns for Flask section. The rest of the docs describe '
+                     'each component of Flask in detail, with a full reference in the API section.',
+          'tags': 'Flask',
+          'date': '23.04.2021'},
+
+         {'author': 'Tom Soyer',
+          'title': 'Jinja is cool',
+          'content': 'Jinja is a fast, expressive, extensible templating engine. Special placeholders in the template '
+                     'allow writing code similar to Python syntax. Then the template is passed data to render the '
+                     'final document.',
+          'tags': 'Jinja',
+          'date': '17.04.2021'},
+
+         {'author': 'Piter Pen',
+          'title': 'Welcome to Werkzeug',
+          'content': 'Werkzeug is a comprehensive WSGI web application library. It began as a simple collection of '
+                     'various utilities for WSGI applications and has become one of the most advanced WSGI utility '
+                     'libraries.',
+          'tags': 'Flask',
+          'date': '25.05.2021'},
+         ]
---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, url_for, redirect
+from articles import posts
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', title='Main page')
+
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html', title='Blog page', posts=posts)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
