28ae56a3dd712ea7da9cad6c49e4f7622166180a
MikesoWeb <klionpsyx@gmail.com>
2021-05-25 19:55:42 +0300

    'add_login'

--- main.py
+++ main.py
@@ -35,9 +35,15 @@ def register():
     return render_template('register.html', form=form, title='Register')
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('blog'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', form=form, title='Login')
 
 
