623202765dfed3f35913647d0e5d51bb180c532d
MikesoWeb <klionpsyx@gmail.com>
2021-05-27 16:00:37 +0300

    'add_flash_validate'

--- blog/forms.py
+++ blog/forms.py
@@ -1,3 +1,4 @@
+from flask import flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
@@ -15,11 +16,13 @@ class RegistrationForm(FlaskForm):
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user:
+            flash('That username is taken. Please choose a different one', 'danger')
             raise ValidationError('That username is taken. Please choose a different one')
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user:
+            flash('That email is taken. Please choose a different one', 'danger')
             raise ValidationError('That email is taken. Please choose a different one')
 
 
