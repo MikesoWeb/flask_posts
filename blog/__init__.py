3a93fdcb5f96295e7c7965f983482719ab7caf3d
MikesoWeb <47977649+MikesoWeb@users.noreply.github.com>
2021-08-16 14:21:31 +0300

    'add_migrate'

--- blog/__init__.py
+++ blog/__init__.py
@@ -4,9 +4,11 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 from blog.config import Config
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
+migrate = Migrate()
 
 login_manager = LoginManager()
 login_manager.login_view = 'users.login'
@@ -21,6 +23,7 @@ def create_app(config_class=Config):
     db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
+    migrate.init_app(app, db, render_as_batch=True)
     mail.init_app(app)
 
 
